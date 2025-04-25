6a17a259b47c20e5bafca07c9ca49ff6071dd113
Gisele-123 <giselemigisha53@gmail.com>
2025-04-25 20:22:57 +0200

    Added the registering and logging in routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router=express.Router()
+const {register, login}= require('../controllers/userController')
 
 router.get('/test', (req,res)=>{
     res.json({message:'Testing api working🎉'});// router to get a given thing, here it's to get this message
 })
 
-// router.post
+router.post('/register', register)
+router.post('/login', login)
 
 module.exports= router;
