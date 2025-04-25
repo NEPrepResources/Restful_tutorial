0a8012f2867b2dbe73cb312b1d925dbc6b7044ce
Gisele-123 <giselemigisha53@gmail.com>
2025-04-25 21:27:22 +0200

    added protection function with token generated after logging in

---
+++ backend/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt= require('jsonwebtoken')
+
+const protect=(req,res,next)=>{
+    const authHeader=req.headers.authorization;
+
+    if(!authHeader || !authHeader.startswith('Bearer ')){
+        return res.status(401).json({
+            error:"No token given"
+        })
+    }
+
+    const token= authHeader.split(" ")[1]
+    try{
+        const decoded=jwt.verify(process.env.JWT_SECRET_KEY);
+        req.user=decoded;
+        next();
+    }catch(err){
+        res.status(401).json({
+            error:'Invalid Token'
+        })
+    }
+}
+
+module.exports=protect
